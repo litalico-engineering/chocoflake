bda3d7f5b6a216aa1a564d348fa6f8a68a5e4e68
Katsumi Kato <katsumi.kato@litalico.co.jp>
2022-03-01 19:13:10 +0900

    :sparkles: Changed Redis operations to atomic processing with Lua scripts.

--- src/Choco/Domain/IdWorker/Redis/IdWorkerOnRedis.php
+++ src/Choco/Domain/IdWorker/Redis/IdWorkerOnRedis.php
@@ -2,10 +2,10 @@
 
 namespace Adachi\Choco\Domain\IdWorker\Redis;
 
+use Adachi\Choco\Domain\IdConfig\IdConfig;
 use Adachi\Choco\Domain\IdValue\Element\RegionId;
 use Adachi\Choco\Domain\IdValue\Element\ServerId;
 use Adachi\Choco\Domain\IdValue\IdValue;
-use Adachi\Choco\Domain\IdConfig\IdConfig;
 use Adachi\Choco\Domain\IdWorker\AbstractIdWorker;
 use Adachi\Choco\Domain\IdWorker\IdWorkerInterface;
 use Predis\Client;
@@ -28,7 +28,7 @@ class IdWorkerOnRedis extends AbstractIdWorker implements IdWorkerInterface
      */
     private $redis;
 
-    const REDIS_SEQUENCE_KEY = 'chocolate_counter';
+    const REDIS_SEQUENCE_PREFIX_KEY = 'chocolate_counter_';
 
     /**
      * @param IdConfig $config
@@ -37,13 +37,13 @@ class IdWorkerOnRedis extends AbstractIdWorker implements IdWorkerInterface
      * @param array $credential
      */
     public function __construct(IdConfig $config,
-                                RegionId $regionId,
-                                ServerId $serverId,
-                                $credential = [
-                                    'scheme'   => 'tcp',
-                                    'host'     => '127.0.0.1',
-                                    'port'     => 6379
-                                ]
+        RegionId $regionId,
+        ServerId $serverId,
+        $credential = [
+            'scheme'   => 'tcp',
+            'host'     => '127.0.0.1',
+            'port'     => 6379
+        ]
     ) {
         $this->config = $config;
         $this->regionId = $regionId;
@@ -58,26 +58,22 @@ class IdWorkerOnRedis extends AbstractIdWorker implements IdWorkerInterface
      */
     public function generate()
     {
+        $script = <<<LUA
+if redis.call('exists', KEYS[1]) == 1 then
+    return redis.call('incr', KEYS[1])
+else
+    redis.call('set', KEYS[1], 0, 'PX', 1000)
+    return 0
+end
+LUA;
+
         $timestamp = $this->generateTimestamp();
+        $sequence = $this->redis->eval($script, 1, self::REDIS_SEQUENCE_PREFIX_KEY . $timestamp);
 
-        $sequence = 0;
-
-        if (! is_null($this->lastTimestamp) && $timestamp->equals($this->lastTimestamp)) {
-            // Get
-            $sequence = $this->redis->incr(self::REDIS_SEQUENCE_KEY) & $this->config->getSequenceMask();
-
-            if ($sequence === 0) {
-                usleep(1000);
-                $timestamp = $this->generateTimestamp();
-            }
-        } else {
-            // Reset sequence if timestamp is different from last one.
-            $sequence = 0;
-            $this->redis->set(self::REDIS_SEQUENCE_KEY, $sequence);
+        if ($sequence !== ($sequence & $this->config->getSequenceMask())) {
+            // Sequence overflowed, rerun
+            return $this->generate();
         }
-
-        // Update lastTimestamp
-        $this->lastTimestamp = $timestamp;
 
         return new IdValue($timestamp, $this->regionId, $this->serverId, $sequence, $this->calculate($timestamp, $this->regionId, $this->serverId, $sequence));
     }
